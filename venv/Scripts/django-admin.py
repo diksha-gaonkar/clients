6290dfa973fa08eb26869c13299a98cf0dc74d5b
Diksha <dgaonkar@unomaha.edu>
2020-09-15 16:45:17 -0400

    Update CMS

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dikga\pycharmprojects\djangoproject\cms_clients\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
